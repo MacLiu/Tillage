141485d9f4c943a297aff9c7740577ccddc35420
Mac Liu <mliu@iastate.edu>
2015-11-22 00:10:05 -0600

    static navbar

--- js/automate.js
+++ js/automate.js
@@ -29,4 +29,17 @@ $(document).ready(function() {
 	}); 
 	return false;
 	});
+
+	  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+	      console.log($(window).scrollTop())
+	    if ($(window).scrollTop() > 280) {
+	      $('#nav').addClass('navbar-fixed');
+	    }
+	    if ($(window).scrollTop() < 281) {
+	      $('#nav').removeClass('navbar-fixed');
+	    }
+	  });
 });
